20614ae27ef0399d122705379eb7e1cab7e9b150
timatom <timothy.a.rollings@gmail.com>
2020-10-01 19:18:42 -0400

    updated the canvas style. Feel this look is better.

--- threejs/js/main-sa.js
+++ threejs/js/main-sa.js
@@ -18,7 +18,37 @@ const resizeCanvas = (canvas) =>
 }
 resizeCanvas(canvas)
 
-ctx.translate(canvas.width / 2.0, canvas.height/8);
+var scale = 0;
+var trans_x = 0;
+var trans_y = 0;
+var color = '';
+
+if (screen.width <= 1440 && screen.width > 800) {
+  scale = 10;
+  trans_x = 1.5;
+  trans_y = 5.5;
+  var color = 'rgba(103, 189, 224, 0.1)';//'rgba(48, 48, 54, 0.05)';
+}
+else if (screen.width <= 800 && screen.width > 620) {
+  scale = 10;
+  trans_x = 2;
+  trans_y = 8;
+  color = 'rgba(103, 189, 224, 0.05)';
+}
+else if (screen.width <= 620 && screen.width > 374) {
+  scale = 10;
+  trans_x = 2;
+  trans_y = 8;
+  color = 'rgba(103, 189, 224, 0.05)';
+}
+else if (screen.width <= 374) {
+  scale = 10;
+  trans_x = 2;
+  trans_y = 8;
+  color = 'rgba(103, 189, 224, 0.025)';
+}
+ctx.translate(canvas.width/trans_x, canvas.height/trans_y);
+
 ctx.globalCompositeOperation = 'lighter'
 ctx.lineWidth = 1
 
@@ -45,8 +75,6 @@ for(var i = 0; i < 10; i++)
   particles.push(new Particle(Math.random()*10, Math.random()*10, Math.random()*10));
 }
 
-let scale = 10
-
 function Lorenz(x, y, z)
 {
   const dt = 0.006;
@@ -63,7 +91,7 @@ const iterate = (time) =>
 {
   for(var i = 0; i < particles.length; i++)
   {
-    ctx.strokeStyle = 'rgba(48, 48, 54, 0.05)';
+    ctx.strokeStyle = color;
     ctx.beginPath();
     ctx.moveTo(particles[i].x*scale, particles[i].z*scale);
     particles[i].update();
